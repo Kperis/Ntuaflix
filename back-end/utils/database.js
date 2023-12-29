c866d9bf937a06e2c01505a3e13067009ef76b72
el20862 <el20862@mail.ntua.gr>
2023-12-29 12:24:13 +0200

    express - nodemon set_up

--- back-end/utils/database.js
+++ back-end/utils/database.js
@@ -2,11 +2,14 @@ const mysql = require('mysql');
 
 /* create connection and export it */
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB,
+    host: 'localhost',
+    user: 'root',
+    database: 'ntuaflix',
+    password: '',
+    port: 3306, // Replace with your actual MySQL port
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = { pool };
