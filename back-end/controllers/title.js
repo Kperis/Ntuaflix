a1eda27395880992eb9aea62d4f3f5680c4510a9
el20862 <el20862@mail.ntua.gr>
2023-12-30 12:02:29 +0200

    Update package.json with new dependencies and titleroute done

--- back-end/controllers/title.js
+++ back-end/controllers/title.js
@@ -1,52 +1,87 @@
-const { pool } = require('../utils/database');
 
-exports.getTitle = (req, res, next) => {
-    const titleID = req.params.titleID; // Assuming the titleID is passed as a parameter in the request
+const setupDatabase = require('../import_imdb_data/connect_database');
 
+
+exports.getTitle = async (req, res, next) => {
+    let titleID = req.params.titleID; // Assuming the titleID is passed as a parameter in the request
+    console.log(titleID);
+
+    if (titleID.startsWith(':')) {
+        titleID = titleID.substring(1);
+    }
+    const pool = await setupDatabase();
     // SQL query with a WHERE clause to filter by titleID
-    const sqlQuery = `
+    const sqlQuery_1 = `
         SELECT
-            tb.tconst AS titleID,
-            tb.titleType AS type,
-            tb.originalTitle,
-            tb.img_url_asset AS titlePoster,
-            tb.startYear,
-            tb.endYear,
-            tb.genres AS genres,
-            ta.title AS akaTitle,
-            ta.region AS regionAbbrev,
-            tp.nconst AS nameID,
-            p.primaryName AS name,
-            tp.category,
-            tr.averageRating AS avRating,
-            tr.numVotes AS nVotes
+            TitleObject.movie_id AS titleID,
+            TitleObject.type AS type,
+            TitleObject.original_title AS originalTitle,
+            TitleObject.image_url AS titlePoster,
+            TitleObject.start_year AS startYear,
+            TitleObject.end_year AS endYear
         FROM
-            TitleBasics tb
-        LEFT JOIN TitleAkas ta ON tb.tconst = ta.titleId
-        LEFT JOIN TitlePrincipals tp ON tb.tconst = tp.tconst
-        LEFT JOIN Person p ON tp.nconst = p.nconst
-        LEFT JOIN TitleRatings tr ON tb.tconst = tr.tconst
+            TitleObject
         WHERE
-            tb.tconst = ?;`;
-
+            TitleObject.movie_id = ?;`;
+    const sqlQuery_2 = `
+        SELECT
+            Genres.genre AS genre
+        FROM
+            Genres
+        WHERE
+            Genres.movie_id = ?;`;
+    const sqlQuery_3 = `
+        SELECT
+            Akas_info.akas_title AS akaTitle,
+            Akas_info.region AS regionAbbrev
+        FROM 
+            Akas_info
+        WHERE
+            Akas_info.movie_id = ?;`;
+    const sqlQuery_4 = `
+        SELECT
+            Ratings.average_rating AS avRating,
+            Ratings.num_votes AS nVotes
+        FROM
+            Ratings
+        WHERE
+            Ratings.movie_id = ?;`;
+    const sqlQuery_5 = `
+        SELECT
+            Contributors.contributor_id AS nameID,
+            Contributors.primary_name AS name,
+            Works.category AS category
+        FROM
+            Contributors
+        INNER JOIN
+            Works
+        ON
+            Contributors.contributor_id = Works.contributor_id
+        WHERE
+            Works.movie_id = ?;`;
     // Execute the query with the titleID as a parameter
-    pool.getConnection((err, connection) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ status: 'failed', message: ['connection string1'] });
-        }
+    try {
+        // Use Promise.all to execute all queries concurrently
+        const [result1, result2, result3, result4, result5] = await Promise.all([
+            pool.query(sqlQuery_1, [titleID]),
+            pool.query(sqlQuery_2, [titleID]),
+            pool.query(sqlQuery_3, [titleID]),
+            pool.query(sqlQuery_4, [titleID]),
+            pool.query(sqlQuery_5, [titleID])
+        ]);
 
-        // Use the acquired connection to execute the query
-        connection.query('USE softeng33;', (err) => {
-            connection.release(); // Release the connection after the query
-
-            if (err) {
-                console.error(err);
-                return res.status(500).json({ status: 'failed', message: ['connection string2'] });
-            }
-
-            // Assuming you want to send the results as JSON
-            res.status(200).json(results);
-        });
-    });
+        // Merge results into a single JSON response
+        const mergedResponse = {
+            titleInfo: result1[0],
+            genres: result2[0],
+            akasInfo: result3[0],
+            ratings: result4[0],
+            contributors: result5[0]
+        };
+        //console.log(mergedResponse);
+        res.json(mergedResponse);
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.sendStatus(500);
+    }
 };
