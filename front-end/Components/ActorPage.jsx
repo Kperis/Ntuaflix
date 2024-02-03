02554a9f07562a2b2ff28cbb86f4f8fc23fc69e5
Konstantinos Perifanos <kperifanos2@gmail.com>
2024-02-03 12:58:10 +0200

    Actor page styled

--- front-end/Components/ActorPage.jsx
+++ front-end/Components/ActorPage.jsx
@@ -1,26 +1,33 @@
 import React from 'react'
 import Image from 'next/image'
 import Movie from './Movie'
+import '@/Styles/actorpage.css'
 
 const ActorPage = ({name, poster, birth, death, profession, movies}) => {
   return (
-    <div>
-        <Image src={poster.replace('{width_variable}', 'w185')} unoptimized alt='actor-photo' width={220} height={270}/>
-        <div>
-            <span>{name}</span>
-            <span>{birth}</span>
-            <span>{death}</span>
-            <span>{profession}</span>
-        </div>
-        <div>
+    <div className='actorpage-container'>
+        <main>
+            <Image className='actor-poster' src={poster.replace('{width_variable}', 'w185')} unoptimized alt='actor-photo' width={440} height={540}/>
+            <section className='actor-info'>
+                <h2>Info:</h2>
+                <span>Name: {name}</span>
+                <span>Birth: {birth}</span>
+                <span>Died: {death}</span>
+                <span>Profession: {profession}</span>
+            </section>
+        </main>
+        <aside className='actor-works'>
+            <h2>Worked on:</h2>
+            <section>
             {
                 movies.map((movie) =>{
                     return(
                         <Movie key={movie.titleID} id={movie.titleID} title={movie.originalTitle} review={movie.rating[0].avRating}/>
                     )
                 })
-            }
-        </div>
+            }   
+            </section>
+        </aside>
     </div>
   )
 }
