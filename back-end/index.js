a8ecdba5583acbbffb1019a1184249a6ff7d868d
Konstantinos Perifanos <kperifanos2@gmail.com>
2024-02-15 21:14:12 +0200

    HTTPS certificate

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,16 @@
 require('dotenv').config(); // using the dotenv package in a Node.js environment
+const https = require('https');
+const fs = require('fs');
 const app = require('./app');
 
+const options = {
+    key: fs.readFileSync('../cert/key.pem'),
+    cert: fs.readFileSync('../cert/certificate.pem')
+};
+
+const server = https.createServer(options, app);
 const PORT = process.env.PORT || 9876;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}!`);
 });
